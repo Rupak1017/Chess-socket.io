9b57080c53a1040922c8b8bf4d3f87eb2e051bc3
rupak07 <rpotdukhe@hawk.iit.edu>
2024-09-08 13:16:15 -0500

    server side logic and start of client side logic

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get("/", (req,res)=>{
 io.on("connection", function (uniquesocket){
     console.log("connected");
    
-
+//Role main logic 
     if(!players.white){
         players.white=uniquesocket.id;
         uniquesocket.emit=("playerRole", "w")
@@ -36,6 +36,45 @@ else if(!players.black){
     players.black=uniquesocket.id;
     uniquesocket.emit=("playerRole", "b")
 }
+else{
+    uniquesocket.emit("spectatorRole");
+}
+
+uniquesocket.on("disconnect", function(){
+    if( uniquesocket.on === players.white){
+        delete players.white;
+    } else{
+        delete players.black;
+    }
+})
+
+
+//this is checking correct person is moving thier turn or now
+uniquesocket.on("move",(move)=>{
+    try{
+ if(chess.turn()==='w' && uniquesocket.id !== players.white) return 
+ if(chess.turn()==='b' && uniquesocket.id !== players.black) return//turn is a method of chess
+
+
+ //this is move logic
+const result = chess.move(move);
+if (result) {
+    currentPlayer = chess.turn();
+    io.emit("move", move)
+    io.emit("boardState", chess.fen())
+}else{
+    console.log("invalid move", move);
+    uniquesocket.emit("invalidMove :",move)
+    
+}
+    }
+    catch(err){
+        console.log(err);
+        uniquesocket.emit("Invalid move :", move);
+        
+
+    }
+})
 
 });
 
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,2 +1,16 @@
 const socket = io(); // most important line to set for frontend from the same domain 
-socket.emit("churan")
+const chess=new Chess();
+const boardElement=document.querySelector(".chessboard")
+
+let draggedPiece=null;
+let sourceSquare=null;
+let playerRole=null;
+
+const renderBoard=()=>{
+    const board= chess.board();
+    boardElement.innerHTML="";
+};
+
+const handlemove=()=>{};
+
+const getPieceUnicode=()=>{};
